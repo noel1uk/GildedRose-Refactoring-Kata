d19b98501a99df57c35b21b1ef5bb763860abb11
Noel Vock <noel1uk@gmail.com>
2018-04-01 20:58:05 +0100

    chore: cleanup

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -9,9 +9,12 @@ class GildedRose
 
   def update_quality()
     @items.each do |item|
+      @is_legendary_item = false
+      @is_cheese = false
+      @is_ticket = false
       type_checker(item)
       if @is_legendary_item
-        return
+        next
       elsif @is_cheese
         if item.quality < 50
           item.quality = item.quality + 1
@@ -20,8 +23,6 @@ class GildedRose
           item.quality = item.quality + 1
         end
         item.sell_in = item.sell_in - 1
-        return
-        #type acts stop here
       elsif @is_ticket
         if item.quality < 50
           item.quality = item.quality + 1
@@ -40,7 +41,6 @@ class GildedRose
           item.quality = item.quality - item.quality
         end
         item.sell_in = item.sell_in - 1
-        return
       else
         if item.sell_in < 0 && item.quality > 1
             item.quality = item.quality - 2
@@ -55,15 +55,12 @@ class GildedRose
 
   def type_checker(item)
     if item.name == 'Aged Brie'
-      #run this code
       @is_cheese = true
     elsif item.name == 'Backstage passes to a TAFKAL80ETC concert'
       @is_ticket = true
-      #run this code
     elsif item.name == 'Sulfuras, Hand of Ragnaros'
       @is_legendary_item = true
     else
-      #run this code
     end
   end
 end
