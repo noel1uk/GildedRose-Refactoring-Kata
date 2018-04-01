c96b9f7d26aa7a579ddcfc6ed97bcb09e298447f
Noel Vock <noel1uk@gmail.com>
2018-04-01 16:56:35 +0100

    refactor: add legendary item instance variable and type checker method. Remove legendary item related if statements

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -2,44 +2,42 @@ class GildedRose
 
   def initialize(items)
     @items = items
+    @legendary_item = false
   end
 
   def update_quality()
     @items.each do |item|
       type_checker(item)
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
-          end
-        end
+      if @legendary_item
+        return
       else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
+        if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
+          if item.quality > 0
+              item.quality = item.quality - 1
+          end
+        else
+          if item.quality < 50
+            item.quality = item.quality + 1
+            if item.name == "Backstage passes to a TAFKAL80ETC concert"
+              if item.sell_in < 11
+                if item.quality < 50
+                  item.quality = item.quality + 1
+                end
               end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
+              if item.sell_in < 6
+                if item.quality < 50
+                  item.quality = item.quality + 1
+                end
               end
             end
           end
         end
-      end
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
+          item.sell_in = item.sell_in - 1
+        if item.sell_in < 0
         if item.name != "Aged Brie"
           if item.name != "Backstage passes to a TAFKAL80ETC concert"
             if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
                 item.quality = item.quality - 1
-              end
             end
           else
             item.quality = item.quality - item.quality
@@ -50,24 +48,20 @@ class GildedRose
           end
         end
       end
+      end
     end
   end
 
   def type_checker(item)
     if item.name == 'Aged Brie'
       #run this code
-      p item.name
     elsif item.name == 'Backstage passes to a TAFKAL80ETC concert'
       #run this code
-      p item.name
     elsif item.name == 'Sulfuras, Hand of Ragnaros'
       #run this code
-      p item.name
+      @legendary_item = true
     else
       #run this code
-      p item.name
     end
   end
 end
-
-# class ItemChecker
